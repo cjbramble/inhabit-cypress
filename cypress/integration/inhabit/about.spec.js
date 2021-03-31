7ce032e59f5b45b95db9c02648558e49a52d44b7
cjbramble <email@chetbramble.com>
2021-03-31 17:29:46 -0400

    Fix pattern and use baseUrl

--- cypress/integration/inhabit/about.spec.js
+++ cypress/integration/inhabit/about.spec.js
@@ -1,11 +1,8 @@
 /// <reference types="cypress" />
 
-context('Contact', () => {
-  beforeEach(() => {
-    cy.visit('https://inhabitiq.com')
-  })
-
-  it('navigates to the About page', () => {
+describe('About', () => {
+  it('Visits the About page', () => {
+    cy.visit('/') // Set in cypress.json
     cy.get('#top-menu > #menu-item-675 > a').click()
     cy.url().should('eq', 'https://inhabitiq.com/about/')
   })
--- cypress/integration/inhabit/contact.spec.js
+++ cypress/integration/inhabit/contact.spec.js
@@ -1,11 +1,9 @@
 /// <reference types="cypress" />
 
-context('Contact', () => {
-  beforeEach(() => {
-    cy.visit('https://inhabitiq.com')
-  })
-
-  it('navigates to the Contact page', () => {
+describe('Contact', () => {
+  it('Visits the Contact page', () => {
+    cy.visit('/')
     cy.get('#top-menu > #menu-item-21 > a').click()
+    // TODO: fill in form. Stub network requests to avoid spamming.
   })
 })
